fb13d47fbea594f568860f0ca18b79ebd424d7ed
dharmendra1702 <109207034+dharmendra1702@users.noreply.github.com>
2023-11-03 20:25:08 +0530

    Add project files.

---
+++ projectdapper.Data/Models/Domain/Person.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projectdapper.Data.Models.Domain
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? serialnumber  { get; set; }
+        [Required]
+        public string? employeename { get; set; }
+        [Required]
+        public string? gender { get; set; }
+        [Required]
+        public string? employeedob { get; set; }
+        [Required]
+        public string? department { get; set; }
+        [Required]
+        public string? designation { get; set; }
+        [Required]
+        public string? mobilenumber { get; set; }
+        [Required]
+        public string? emailid { get; set; }
+        [Required]
+        public string? panno { get; set; }
+        [Required]
+        public string? aadharno { get; set; }
+        [Required]
+        public string? dateofissue { get; set; }
+        [Required]
+        public string? expirydata { get; set; }
+        [Required]
+        public string? dataofapplication { get; set; }
+        [Required]
+        public string? dscno { get; set; }
+        [Required]
+        public string? status { get; set; }
+
+    }
+}
---
+++ projectdapper.UI/Controllers/PersonController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projectdapper.Data.Models.Domain;
+using projectdapper.Data.Repository;
+
+namespace projectdapper.UI.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly IPersonRepository _personRepo;
+
+        public PersonController(IPersonRepository personRepo)
+        {
+            _personRepo = personRepo;
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Person person)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                    return View(person);
+                bool addPersonResult = await _personRepo.AddAsync(person);
+                if (addPersonResult)
+                    TempData["msg"] = "Successfully added";
+                else
+                    TempData["msg"] = "Could not added";
+            }
+            catch (Exception)
+            {
+                TempData["msg"] = "Could not added";
+            }
+            return RedirectToAction(nameof(Add));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var person = await _personRepo.GetByIdAsync(id);
+            //if (person == null)
+            //    return NotFound();
+            return View(person);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Person person)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View(person);
+                var updateResult = await _personRepo.UpdateAsync(person);
+                ViewBag.Person = person;
+                if (updateResult)
+                    TempData["msg"] = "Updated succesfully";
+                else
+                    TempData["msg"] = "Could not updated";
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = "Could not updated";
+            }
+            return View(person);
+        }
+        public async Task<IActionResult> DisplayAll()
+        {
+            var people = await _personRepo.GetAllAsync();
+            return View(people);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleteResult = await _personRepo.DeleteAsync(id);
+            return RedirectToAction(nameof(DisplayAll));
+        }
+    }
+}
